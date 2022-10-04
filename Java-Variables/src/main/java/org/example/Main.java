5561370a0ee74dbd25ef5127d6af77c20cdda547
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-10-04 10:24:14 +0300

    Learn Java Variables

---
+++ Java-Variables/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        byte b =22;
+        short s=120;
+        int x=10;
+        long longint=123123123123123L;
+        float pi=3.1415234f;
+        double pi_ex=3.14535335353;
+        char sum ='+';
+        String name="sedat";
+
+        // Adlandırma Kuralı (camelCase)
+        String kullaniciAdi ="sedatb";
+
+
+
+        System.out.println("Hello world! "+name);
+    }
+}
